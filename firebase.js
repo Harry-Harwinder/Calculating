61c03874bac65294741b7b957f40ac10be9537ac
Harwinder <harryghatorey@gmail.com>
2024-06-12 15:37:39 +0530

    update firebase code

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDK7fTcGy0LgTfTr89sezbjjTelWU0eYOc",
+  authDomain: "calculating-5e63d.firebaseapp.com",
+  projectId: "calculating-5e63d",
+  storageBucket: "calculating-5e63d.appspot.com",
+  messagingSenderId: "1068273186202",
+  appId: "1:1068273186202:web:5358d6e36f888d43217623",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
